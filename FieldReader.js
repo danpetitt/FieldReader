87bb4414e3c0bcf0eab6b3abda4eaa8c2c782c0c
Dan Petitt <danp@coderanger.com>
2014-05-09 17:35:05 +0100

    Added copyright

--- FieldReader.js
+++ FieldReader.js
@@ -1,53 +1,29 @@
-/*
-This object was created to parse file data saved from an Excel spreadsheet in CSV or TSV format.
-
-Just create a new FieldReader object passing a field and line delimiter character, then throw characters at it.
-
-Once all the data is parsed, call 'end()' and then you can get at the field, row information.
-
-Methods:
-gotChar( character );  // each character from the file is passed in using this method
-end();                 // when all data is passed in, call 'end'
-
-rowCount();            // get total number of rows retrieved
-fieldCount( row );     // get total number of fields for a given row (zero indexed)
-field( row, field );   // get field data for given row and field index (zero indexed)
-
-Example:
-var data = "test1\t\"test 2\nsecond row in cell\"\t\"test 3, oh oh \"\"aha\"\"\"\t8328\t\"\"\"test4 multiples\"\"\"\ntest1b\t\"test 2b\nsecond row in cell\"\t\"test 3b, oh oh \"\"aha\"\"\"\t99\t\"\"\"test4b multiples\"\"\"\ntest1c\t\"test 2c\nsecond row in cell\"\t\"test 3c, oh oh \"\"aha\"\"\"\t14\t\"test4c multiples\"";
-
-var parser = new FieldReader( "\t", "\n" );
-for( var n = 0; n < data.length; n++ )
-{
-  parser.gotChar( data.charAt( n ) );
-}
-parser.end();
-
-
-// Setup test using: https://gist.github.com/coderangerdan/10770050
-var tester = new SimpleTest();
-tester.equal( parser.rowCount(), 3 );
-tester.equal( parser.field( 0, 0 ), "test1" );
-tester.equal( parser.field( 0, 1 ), "test 2\nsecond row in cell" );
-tester.equal( parser.field( 0, 2 ), "test 3, oh oh \"aha\"" );
-tester.equal( parser.field( 0, 3 ), "8328" );
-tester.equal( parser.field( 0, 4 ), "\"test4 multiples\"" );
-
-tester.equal( parser.field( 1, 0 ), "test1b" );
-tester.equal( parser.field( 1, 1 ), "test 2b\nsecond row in cell" );
-tester.equal( parser.field( 1, 2 ), "test 3b, oh oh \"aha\"" );
-tester.equal( parser.field( 1, 3 ), 99 );
-tester.equal( parser.field( 1, 4 ), "\"test4b multiples\"" );
-
-tester.equal( parser.field( 2, 0 ), "test1c" );
-tester.equal( parser.field( 2, 1 ), "test 2c\nsecond row in cell" );
-tester.equal( parser.field( 2, 2 ), "test 3c, oh oh \"aha\"" );
-tester.equal( parser.field( 2, 3 ), 14 );
-tester.equal( parser.field( 2, 4 ), "test4c multiples" );
-
-tester.notEqual( parser.field( 2, 3 ), "test4c multipless" );
-tester.finish();
-*/
+/**
+ * FieldReader - A simple JavaScript object to to parse file data saved from an Excel spreadsheet in CSV or TSV format
+ * @author Dan Petitt <danp@coderanger.com>
+ * Homepage: https://github.com/coderangerdan/FieldReader
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2014 Dan Petitt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 var FieldReader = function( fieldDelimiterCharacter, lineDelimiterCharacter )
 {
@@ -83,15 +59,15 @@ var FieldReader = function( fieldDelimiterCharacter, lineDelimiterCharacter )
     lineDelimiter : function( delimiterCharacter ) {
       _lineDelimiter = delimiterCharacter;
     },
-    
+
     // return number of rows parsed
     rowCount : function() { return _rows.length; },
-    
+
     // rows and fields are zero indexed
     field : function( row, field ) {
       return _rows[ row ][ field ];
     },
-    
+
     // rows are zero indexed
     fieldCount : function( row ) {
       return _rows[ row ].length;
@@ -119,7 +95,7 @@ var FieldReader = function( fieldDelimiterCharacter, lineDelimiterCharacter )
             _inQuotes = !_inQuotes;
           }
           break;
- 
+
         case _fieldDelimiter:
           if( !_inQuotes )
           {
@@ -131,7 +107,7 @@ var FieldReader = function( fieldDelimiterCharacter, lineDelimiterCharacter )
             _currentField += _lastChar;
           }
           break;
-                                               
+
         case _lineDelimiter:
           if( !_inQuotes )
           {
@@ -146,14 +122,14 @@ var FieldReader = function( fieldDelimiterCharacter, lineDelimiterCharacter )
             _currentField += _lastChar;
           }
           break;
-          
+
         default:
           _currentField += _lastChar;
       }
-   
+
       _lastChar = char;
     },
-    
+
     // call end to finish parsing
     end : function() {
       if( _lastChar.length > 0 )
